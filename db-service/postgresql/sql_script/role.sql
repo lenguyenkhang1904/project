70f6f0eb5a7f2d80e6827c924b4ebfd10e8b904c
Nguyên Khang <79681550+lenguyenkhang1904@users.noreply.github.com>
2023-09-23 09:54:21 +0700

    hfwifwef

--- db-service/postgresql/sql_script/role.sql
+++ db-service/postgresql/sql_script/role.sql
@@ -1,8 +1,8 @@
 WITH role_values AS ( 
 SELECT 
 'admin-01b2-11ee-be56-0242ac120002' AS id,
-'ADMINISTRATOR' AS description,
-'administrator' AS name
+'administrator' AS description,
+'ADMINISTRATOR' AS name
 )
 DELETE FROM role WHERE id = (SELECT CAST(id AS TEXT) FROM role_values);
 WITH role_values AS ( 
@@ -45,23 +45,6 @@ SELECT
 'register-and-learner-01b2-11ee-be56-0242ac120002' AS id,
 'register-and-learner' AS description,
 'REGISTERANDLEARNER' AS name
-)
-INSERT INTO role(id, description, name) 
-SELECT id, description, name
-FROM role_values;
-
-WITH role_values AS ( 
-SELECT 
-'staff-01b2-11ee-be56-0242ac120002' AS id,
-'staff' AS description,
-'STAFF' AS name
-)
-DELETE FROM role WHERE id = (SELECT CAST(id AS TEXT) FROM role_values);
-WITH role_values AS ( 
-SELECT 
-'staff-01b2-11ee-be56-0242ac120002' AS id,
-'staff' AS description,
-'STAFF' AS name
 )
 INSERT INTO role(id, description, name) 
 SELECT id, description, name
